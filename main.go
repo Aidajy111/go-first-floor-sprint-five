6e1902132a78c0eecbf8ae6003830b045e181bcd
Aidajy111 <aidasjy4.00@gmail.com>
2025-02-08 21:11:19 +0800

    Error correction

--- main.go
+++ main.go
@@ -34,6 +34,7 @@ func (t Training) distance() float64 {
 // meanSpeed возвращает среднюю скорость бега или ходьбы.
 func (t Training) meanSpeed() float64 {
 	// вставьте ваш код ниже
+
 	if t.Duration == 0 {
 		return 0
 	}
